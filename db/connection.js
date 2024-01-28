3af5364ab97d0f0dd143aefcebbded9e8e3ff063
Simran Yadav <simranyadav464@gmail.com>
2024-01-28 19:05:26 +0530

    Added the files

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,11 @@
 require("dotenv").config();
-
 const mongoose=require("mongoose");
 
 mongoose.connect(process.env.DB_URI ,{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
-    console.log("Connection is established");
+    console.log("Connection is established successfully");
 }).catch((e)=>{
     console.log(e)
     console.log("No Connection is Found");
